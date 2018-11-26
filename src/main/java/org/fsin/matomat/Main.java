9bcd399258b41eb6dac3b53c632e99c2a489efd8
Christian Schabesberger <chris.schabesberger@mailbox.org>
2018-11-26 11:02:27 +0100

    add jdbc connection to mysql database

--- src/main/java/org/fsin/matomat/Main.java
+++ src/main/java/org/fsin/matomat/Main.java
@@ -1,7 +1,17 @@
 package org.fsin.matomat;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] argv) {
         System.out.println("hallo welt");
+        try {
+            Connection connect = DriverManager
+                    .getConnection("jdbc:mysql://127.0.0.1/matohmat", "root", "root");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
