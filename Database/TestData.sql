812400c6e634113f25ec9d0739482ca829e1ba31
Christian Schabesberger <chris.schabesberger@mailbox.org>
2018-11-27 18:52:52 +0100

    add test data

--- Database/TestData.sql
+++ Database/TestData.sql
@@ -13,10 +13,26 @@ VALUES
        (1, 'himler', 'christopf', 'himler@finanzen.urg', 'aasdfasfd', 2);
 
 INSERT INTO Products
-    (ID, name, price, stock, image_url, reorder_point, available)
+    (ID, name, price, stock, image_url, reorder_point, available, product_hash)
 VALUES
-       (1, 'Mate', '0.70', 200, 'https://matebild.com/bild.jpg', 50, 1),
-       (2, 'Snickers', '0.20', 300, 'https://snickersbild.com/bild.jpg', 20, 1),
-       (3, 'Mars', '0.20', 0, 'https://marsbild.com/bild.jpg', 50, 0),
-       (4, 'Wasser', '0.50', 130, 'https://lolbild.com/bild.jpg', 70, 1);
+       (1, 'Mate', '0.70', 200, 'https://matebild.com/bild.jpg', 50, 1, 'aksdjfkj'),
+       (2, 'Snickers', '0.20', 300, 'https://snickersbild.com/bild.jpg', 20, 1, 'laksdjfje'),
+       (3, 'Mars', '0.20', 0, 'https://marsbild.com/bild.jpg', 50, 0, 'fi3jjf'),
+       (4, 'Wasser', '0.50', 130, 'https://lolbild.com/bild.jpg', 70, 1, 'eieiiej');
 
+
+INSERT INTO Transactions
+    (ID, Date, Type, charged_amount, sender, recipient)
+VALUES
+       (1, '2014-12-18 05:17:17', 'T', 0, 1, 2),
+       (2, '2014-12-18 14:17:17', 'T', 0, 3, 2),
+       (3, '2014-12-18 15:17:17', 'I', 10.00, 4, 2),
+       (4, '2014-12-18 18:17:17', 'O', 10.00, 3, 2);
+
+INSERT INTO transaction_amount_products
+    (Transactions_ID, Products_ID, count)
+VALUES
+       (1, 1, 2),
+       (1, 2, 1),
+       (2, 3, 1),
+       (2, 4, 4);
