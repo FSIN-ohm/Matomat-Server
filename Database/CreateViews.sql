9f751cce1b590d8925379b7034a8dce7fd66e090
Christian Schabesberger <chris.schabesberger@mailbox.org>
2018-12-04 10:39:30 +0100

    add stock view and move to mariadb

--- Database/CreateViews.sql
+++ Database/CreateViews.sql
@@ -1,3 +1,5 @@
+USE matohmat;
+
 DROP VIEW IF EXISTS charges_for_purchases;
 CREATE VIEW charges_for_purchases AS
   SELECT P.Transaction_ID as ID,
@@ -32,27 +34,56 @@ CREATE VIEW transfer_transactions AS
 
 DROP VIEW IF EXISTS users_expense;
 CREATE VIEW users_expense AS
-  SELECT U.ID,
+  SELECT T.sender AS user_id,
          SUM(IFNULL(Tr.charged_amount, 0) + IFNULL(cfp.charged_amount, 0)) AS expense
-    FROM Users U
-      LEFT JOIN Transactions T on U.ID = T.sender
+    FROM Transactions T
       LEFT JOIN Transfers Tr ON T.ID = Tr.transaction_ID
       LEFT JOIN charges_for_purchases cfp on T.ID = cfp.ID
-        GROUP BY U.ID;
+        GROUP BY T.sender;
 
 DROP VIEW IF EXISTS users_income;
 CREATE VIEW users_income AS
-  SELECT U.ID,
+  SELECT T.recipient AS user_id,
          SUM(IFNULL(Tr.charged_amount, 0) + IFNULL(cfp.charged_amount, 0)) AS income
-      FROM Users U
-        LEFT JOIN Transactions T on U.ID = T.recipient
+      FROM Transactions T
         LEFT JOIN Transfers Tr ON T.ID = Tr.transaction_ID
         LEFT JOIN charges_for_purchases cfp on T.ID = cfp.ID
-          GROUP BY U.ID;
+          GROUP BY T.recipient;
 
 DROP VIEW IF EXISTS users_balance;
 CREATE VIEW users_balance AS
-  SELECT iou.ID,
-         iou.income - eou.expense AS balance
-    FROM income_of_users iou
-      JOIN expenses_of_users eou ON iou.ID = eou.ID;
+  SELECT U.ID,
+         IFNULL(ui.income, 0) - IFNULL(ue.expense, 0) AS balance
+    FROM Users U
+        LEFT JOIN users_income ui ON U.ID = ui.user_id
+        LEFT JOIN users_expense ue ON U.ID = ue.user_id;
+
+
+DROP VIEW if exists bought_products;
+CREATE VIEW bought_products AS
+  SELECT T.sender as user_id,
+         P.Product_ID as product_id,
+         SUM(P.count) as bought_products
+  From Transactions T
+  JOIN Purchases P on T.ID = P.Transaction_ID
+  GROUP BY P.Product_ID, T.sender;
+
+DROP VIEW if exists sold_products;
+CREATE VIEW sold_products AS
+  SELECT T.recipient as user_id,
+         P.Product_ID as product_id,
+         SUM(P.count) as sold_products
+  From Transactions T
+  JOIN Purchases P on T.ID = P.Transaction_ID
+  GROUP BY P.Product_ID, T.recipient;
+
+DROP VIEW IF EXISTS product_stocks;
+CREATE VIEW product_stocks AS
+  SELECT U.ID as user_id,
+         IFNULL(bp.product_id, sp.product_id) as product_id,
+         IFNULL(bp.bought_products, 0) - IFNULL(sp.sold_products, 0) as stock
+    FROM Users U
+    LEFT JOIN bought_products bp ON bp.user_id = U.ID
+    LEFT JOIN sold_products sp ON bp.user_id = sp.user_id AND sp.product_id = bp.product_id
+      WHERE NOT ISNULL(IFNULL(bp.product_id, sp.product_id))
+        AND IFNULL(bp.bought_products, 0) - IFNULL(sp.sold_products, 0) > 0;
