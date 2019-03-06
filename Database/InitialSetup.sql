9ec9749e8c6c8f25a183fd244d3cd979601e939e
Christian Schabesberger <chris.schabesberger@mailbox.org>
2019-03-06 16:03:44 +0100

    update database add product hash for qr code scanner

--- Database/InitialSetup.sql
+++
@@ -1,3 +0,0 @@
-CALL user_create(SHA1('world'));
-CALL user_create(SHA1('dagobert'));
-CALL admin_create("Admin", "fachsachft-in@th-nuernberg.de", sha1( concat("keinpasswort", sha1("keinsalt") ), sha1("keinsalt") ) );
