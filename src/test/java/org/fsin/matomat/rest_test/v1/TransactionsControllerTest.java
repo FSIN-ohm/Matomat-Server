2de77a1c0f9ee5020484aff8dd1f1a6f73513dcb
Christian Schabesberger <chris.schabesberger@mailbox.org>
2019-04-22 23:59:17 +0200

    do magic stuff

--- src/test/java/org/fsin/matomat/rest_test/v1/TransactionsControllerTest.java
+++ src/test/java/org/fsin/matomat/rest_test/v1/TransactionsControllerTest.java
@@ -1,12 +1,29 @@
 package org.fsin.matomat.rest_test.v1;
 
+import org.fsin.matomat.database.Database;
+import org.fsin.matomat.rest.v1.TransactionsController;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 public class TransactionsControllerTest {
-    WebTestClient client;
+
+    @Autowired
+    private static WebTestClient client;
 
     @BeforeClass
-    void init() {
-        client = WebTestClient.bindToController(new TestController()).build();
+    public static void setup() {
+        Database.init("127.0.0.1", "matohmat", "root", "root");
+        client = WebTestClient.bindToController(new TransactionsController()).build();
     }
+
+    @Test
+    public void testGetTransactions() {
+        client.get().uri("/v1/transactions")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().jsonPath("[3993993].jsonpath_is_mother_ufcking_bullshit", 48949);
+    }
+
 }
