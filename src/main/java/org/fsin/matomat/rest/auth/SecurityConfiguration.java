5ffd001b9a1e9aebc16f0b3300697eb4b20bd32f
Christian Schabesberger <chris.schabesberger@mailbox.org>
2019-05-13 14:03:39 +0200

    disabled csrf because of posts

--- src/main/java/org/fsin/matomat/rest/auth/SecurityConfiguration.java
+++ src/main/java/org/fsin/matomat/rest/auth/SecurityConfiguration.java
@@ -24,6 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http)
         throws Exception {
         http
+                .csrf().disable()
                 .cors()
                 .and()
                 .sessionManagement()
