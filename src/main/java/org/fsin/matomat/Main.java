3839b3bf243d8f817803eb0e748c9fc4ae9a1f6a
Christian Schabesberger <chris.schabesberger@mailbox.org>
2019-06-02 00:22:18 +0200

    add test email

--- src/main/java/org/fsin/matomat/Main.java
+++ src/main/java/org/fsin/matomat/Main.java
@@ -7,6 +7,7 @@ import org.fsin.matomat.rest.auth.Authenticator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,12 +21,25 @@ public class Main {
      * @param argv for debuging should be "127.0.0.1 matohmat matomat_system password_here device_keys.txt null /"
      */
     public static void main(String[] argv) {
+        Mailer mailer = new Mailer();
         final String configFile;
-        if(argv.length != 1) {
+        if(argv.length < 1) {
             System.err.println("In order to start the server you need to provide a location for the config file.");
             configFile = "./server.conf";
         } else {
-            configFile = argv[0];
+            switch(argv[0]) {
+                case "--test-mail":
+                    configFile = argv[1];
+                    mailTest(configFile);
+                    break;
+                case "--help":
+                    configFile = argv[0];
+                    System.out.println("matohmat [--test-mail] [--help] [server.conf]");
+                    System.exit(0);
+                    break;
+                default:
+                    configFile = argv[0];
+            }
         }
 
         try{
@@ -42,6 +56,8 @@ public class Main {
             Authenticator.init(deviceKeys);
 
             System.setProperty("server.servlet.context-path", contextPath);
+
+
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Could not init server. Maybe the configuration is not correct.");
@@ -49,8 +65,7 @@ public class Main {
             System.exit(1);
         }
 
-        Mailer mailer = new Mailer();
-        mailer.sendMessage("chris.schabesberger@mailbox.org", "gurken");
+
 
         SpringApplication.run(Main.class, argv);
     }
@@ -65,5 +80,19 @@ public class Main {
                         .allowedMethods("POST", "GET", "OPTIONS", "DELETE", "PATCH");
             }
         };
+    }
+
+    private static void mailTest(String configFile) {
+        Configurator.init(configFile);
+        Configurator conf = Configurator.getInstance();
+        final String dbHost = conf.getValueString("db_host");
+        final String schema = conf.getValueString("db_schema");
+        final String dbUser = conf.getValueString("db_user");
+        final String dbPwd = conf.getValueString("db_password");
+
+        Mailer mailer = new Mailer();
+        Database.init(dbHost, schema, dbUser, dbPwd);
+        mailer.sendTestMessage();
+        System.exit(0);
     }
 }
--- src/main/java/org/fsin/matomat/inventory_watch/Mailer.java
+++ src/main/java/org/fsin/matomat/inventory_watch/Mailer.java
@@ -1,17 +1,108 @@
 package org.fsin.matomat.inventory_watch;
 
 import org.fsin.matomat.Configurator;
+import org.fsin.matomat.database.Database;
+import org.fsin.matomat.database.model.AdminEntry;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
 import java.util.Properties;
 
 public class Mailer {
 
-    public void sendMessage(String recipient, String mesg) {
+    public class CratesPerProduct {
+        String product;
+        int crates;
+
+        public CratesPerProduct(String product, int crates) {
+            this.product = product;
+            this.crates = crates;
+        }
+
+        public String getProduct() {
+            return product;
+        }
+
+        public int getCrates() {
+            return crates;
+        }
+    }
+
+    public void sendTestMessage() {
+        sendReorderMessage("testProduct",
+                new CratesPerProduct[] {
+                        new CratesPerProduct("testProduct", 1337),
+                        new CratesPerProduct("pink cream", 73),
+                        new CratesPerProduct("soilent green", 42)
+                });
+    }
+
+    public void sendReorderMessage(String productOutOfStock, CratesPerProduct[] cratesPerProduct) {
+        try {
+            Database db = Database.getInstance();
+            List<AdminEntry> admins = db.adminGetAll(0, 10000, true);
+            Configurator config = Configurator.getInstance();
+
+            String message = buildMessage(
+                    loadTemplate(config.getValueString("mail_template_file")),
+                    productOutOfStock, cratesPerProduct);
+
+            String subject = config.getValueString("mail_subject")
+                    .replace("<{product}>", productOutOfStock);
+
+            for(AdminEntry admin : admins) {
+                try {
+                    sendMessage(admin.getEmail(), message, subject);
+                } catch (Exception e)  {
+                    System.err.println("Could not send message to admin: "
+                            + admin.getUsername()
+                            + " with address: "
+                            + admin.getEmail());
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Could not send mail to any admins:");
+            e.printStackTrace();
+        }
+
+    }
+
+    private String loadTemplate(String fileName) {
+        try {
+            BufferedReader file = new BufferedReader(new FileReader(fileName));
+            String line;
+            StringBuilder msg = new StringBuilder();
+            while((line = file.readLine()) != null) {
+                msg.append(line);
+                msg.append("\n");
+            }
+            return msg.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not load mail template", e);
+        }
+    }
+
+    private String buildMessage(String template, String product, CratesPerProduct[] cratesPerProduct) {
+
+        StringBuilder productCrateList = new StringBuilder();
+        for(CratesPerProduct cpp : cratesPerProduct) {
+            productCrateList.append(cpp.getProduct());
+            productCrateList.append(":\t");
+            productCrateList.append(cpp.getCrates());
+            productCrateList.append("\n");
+        }
+        return template.replace("<{product}>", product)
+                .replace("<{product_crate_list}>", productCrateList.toString());
+    }
+
+    private void sendMessage(String recipient, String mesg, String subject) {
         try {
             final Configurator config = Configurator.getInstance();
 
@@ -36,7 +127,7 @@ public class Mailer {
             message.setRecipients(
                     Message.RecipientType.TO,
                     InternetAddress.parse(recipient));
-            message.setSubject("Mate is empty");
+            message.setSubject(subject);
             MimeBodyPart msgBody = new MimeBodyPart();
             msgBody.setContent(mesg, "text/plain");
             message.setContent( new MimeMultipart(msgBody));
